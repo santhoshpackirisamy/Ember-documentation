f92af093389010a53eca63766b718127ca6a6c31
santhoshpackirisamy <santhoshpackirisamy007@gmail.com>
2020-03-03 15:44:53 +0530

    computed properties and aggregate data

--- app/routes/demo.js
+++ app/routes/demo.js
@@ -1,101 +1,49 @@
+import { A } from '@ember/array';
 import EmberObject, { computed } from '@ember/object';
+import Component from '@ember/component';
 
-const Person = EmberObject.extend({
-  // these will be supplied by `create`
-  firstName: null,
-  lastName: null,
-  age: null,
-  country: null,
+export default Component.extend({
+  todos: null,
 
-  fullName: computed('firstName', 'lastName', function() {                     // ***************  compute during the call ************
-    console.log('compute fullName');    // track when the property recomputes
-    return `${this.firstName} ${this.lastName}`;
-  }),
-    
-  description: computed('fullName', 'age', 'country', function() {             // ***************  dynamic update fulname value ***********
-    return `${this.fullName} Age: ${this.age}; Country: ${this.country}`;
-  })
-
-});
-
-let ironMan = Person.create({
-  firstName: 'Tony',
-  lastName:  'Stark'
-});
-
-console.log(ironMan.fullName);                       // 'compute fullName'
-ironMan.set('firstName', 'Bruce')                                           // *****************  only use set to change ******************** 
-
-console.log(ironMan.fullName);                       // 'compute fullName'
-console.log(ironMan.fullName);                       // no console output since dependencies have not changed
-
-
-
-const Home = EmberObject.extend({
-  location: {
-    streetName: 'Evergreen Terrace',
-    streetNumber: 742
+  init() {
+    this._super(...arguments);
+    this.set('todos', A([
+      EmberObject.create({ title: 'Buy food', isDone: true }),
+      EmberObject.create({ title: 'Eat food', isDone: false }),
+      EmberObject.create({ title: 'Catalog Tomster collection', isDone: true }),
+    ]));
   },
 
-//  address: computed('location', function() {                                
-//  address: computed('location.{streetName,streetNumber}', function() {    
-  address: computed('location.streetName', 'location.streetNumber', function() {
-    return `${this.location.streetNumber} ${this.location.streetName}`;    // ************* compute for object properties *************
-  })
-});
-
-let home = Home.create()
-
-console.log(home.address) // 742 Evergreen Terrace
-home.set('location.streetNumber', 744)
-console.log(home.address) // 744 Evergreen Terrace
-
-
-let captainAmerica = Person.create({
-  firstName: 'Steve',
-  lastName: 'Rogers',
-  age: 80,
-  country: 'USA'
-});
-
-
-console.log(captainAmerica.get('description')); // "Steve Rogers; Age: 80; Country: USA"
-
-captainAmerica.set('firstName', 'William');
-
-console.log(captainAmerica.description); // "William Rogers; Age: 80; Country: USA"
-
-
-
-const Persons = EmberObject.extend({
-  firstName: null,
-  lastName: null,
-
-  fullName: computed('firstName', 'lastName', {
-    get(key) {
-      return `${this.firstName} ${this.lastName}`;
-    },
-    set(key, value) {                                   // *********** set value by compute ************
-      let [firstName, lastName] = value.split(/\s+/);
-      this.set('firstName', firstName);
-      this.set('lastName',  lastName);
-      return value;
-    }
+  titles: computed('todos.[]', function() {             // *********** [] - detect the inc or dec or replaced in array value *************
+    return this.todos.mapBy('title');
   })
 });
 
 
-let captain = Persons.create();
-captain.set('fullName', 'William Burnside');
-console.log(captain.firstName); // William
-console.log(captain.lastName); // Burnside
+export default Component.extend({
+  todos: null,
+
+  init() {
+    this._super(...arguments);
+    this.set('todos', A([
+      EmberObject.create({ isDone: true }),
+      EmberObject.create({ isDone: false }),
+      EmberObject.create({ isDone: true }),
+    ]));
+  },
+
+  incomplete: computed('todos.@each.isDone', function() {   // *********** @ - detect the changes in array value *************
+    let todos = this.todos;
+    return todos.filterBy('isDone', false);
+  })
+});
 
 
-//  ************************* Computed property macros ************************
+incomplete: computed('todos.[]', 'todos.@each.isDone', function() {
+...
+})
 
-//
-//  isIronManLongWay: computed('fullName', function() {
-//    return this.fullName === 'Tony Stark';
-//  }),
-//
-//  isIronManShortWay: equal('fullName', 'Tony Stark')
+
+// *************************  macros ****************
+
+incomplete: filterBy('todos', 'isDone', false)  // @
